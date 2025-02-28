bea7897aa9526f37a11f2cd50ae9dfef54a4d147
ShodiyorShukurov <shodiyorshukurov07@gmail.com>
2025-02-28 15:27:42 +0500

    Deeplink add

--- src/App.js
+++ src/App.js
@@ -1,32 +1,39 @@
-import { Route, Routes, Navigate } from "react-router-dom";
-import "antd/dist/antd.css";
-import "./assets/styles/main.css";
-import "./assets/styles/responsive.css";
-import PrivateRoute from "./utils/PrivateRoute";
+import { Route, Routes, Navigate } from 'react-router-dom';
+import 'antd/dist/antd.css';
+import './assets/styles/main.css';
+import './assets/styles/responsive.css';
+import PrivateRoute from './utils/PrivateRoute';
+import LoginPage from './components/layout/Login';
+import UsersListTable from './pages/UserList/UsersListTable';
+import DevicesListTable from './pages/DevicesList/DevicesListTable';
+import CategoryListTable from './pages/CategoryList/CategoryListTable';
+import ArticlesListTable from './pages/ArticlesList/ArticlesListTable';
 
-import LoginPage from "./components/layout/Login";
+function DeepLinkPage() {
+  const { id } = useParams();
 
-import UsersListTable from "./pages/UserList/UsersListTable";
-import DevicesListTable from "./pages/DevicesList/DevicesListTable";
-import CategoryListTable from "./pages/CategoryList/CategoryListTable";
-import ArticlesListTable from "./pages/ArticlesList/ArticlesListTable";
+  return (
+    <div>
+      <h2>Deep Link Ochildi! Parametr: {id}</h2>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        {/* Login page */}
         <Route path="/login" element={<LoginPage />} />
 
-        {/* Redirect root to login */}
         <Route path="/" element={<Navigate to="/login" />} />
 
-        {/* Private route logic for protecting routes */}
+        <Route path="/maqola/:id" element={<DeepLinkPage />} />
+
         <Route path="/" element={<PrivateRoute />}>
           <Route path="users-list" element={<UsersListTable />} />
           <Route path="devices-list" element={<DevicesListTable />} />
           <Route path="categories-list" element={<CategoryListTable />} />
-          <Route path='articles-list' element={<ArticlesListTable />} />
+          <Route path="articles-list" element={<ArticlesListTable />} />
         </Route>
       </Routes>
     </div>
